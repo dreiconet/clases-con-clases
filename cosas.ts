5493b4c0e73b8968198c0d3080994111ed404358
dreiconet <dreiconet@gmail.com>
2022-05-31 20:40:38 -0300

    primer commit

---
+++ cosas.ts
@@ -0,0 +1,21 @@
+
+
+const pelis = [
+  {titulo:"hola", rating: 4, id:1},
+  {titulo: "peli 2", rating: 3, id:2},
+];
+
+function getAll(){
+  return pelis;
+}
+
+function getByid(id: number){
+ const peliEncontrada = pelis.find( (p) => {
+   return p.id == id;
+  });
+
+  return peliEncontrada;
+
+}
+export {getAll, getByid};
+
---
+++ index.ts
@@ -0,0 +1,9 @@
+import { getAll, getByid } from "./cosas";
+
+
+function main(){
+  console.log(getAll());
+  console.log(getByid(2));
+}
+
+main();
